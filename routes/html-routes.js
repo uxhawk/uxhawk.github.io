ab3ced57e22990be66fbaa9bda96816a864e19cb
David Hawkins <uxhawk@gmail.com>
2020-05-22 11:14:10 -0400

    fixed footer and created 404 routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,15 @@ const path = require('path');
 
 
 module.exports = function(app) {
-  app.get('/', function(req, res) {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
+  });
+
+  app.get('/ux-ui', (req, res) => {
+    res.sendFile(path.join(__dirname, '../ux-ui.html'));
+  });
+
+  app.get('/web-dev', (req, res) => {
+    res.sendFile(path.join(__dirname, '../web-dev.html'));
   });
 };
