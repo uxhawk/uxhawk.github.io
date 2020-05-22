acc89db956f7987d91d6745ddbd6925ec23318dc
David Hawkins <uxhawk@gmail.com>
2020-05-22 14:02:44 -0400

    added android and onboarding handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,4 +14,12 @@ module.exports = function(app) {
   app.get('/web-dev', (req, res) => {
     res.sendFile(path.join(__dirname, '../web-dev.html'));
   });
+
+  app.get('/android-app', (req, res) => {
+    res.render('android-app');
+  });
+
+  app.get('/new-customer-onboarding', (req, res) => {
+    res.render('new-customer-onboarding');
+  });
 };
