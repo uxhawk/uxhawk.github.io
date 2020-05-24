a135a818e26844eedc9a82f578cf5bbec32a37ee
David Hawkins <uxhawk@gmail.com>
2020-05-24 11:40:02 -0400

    handlebar views for each page and html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,15 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require('path');
-
-
 module.exports = function(app) {
   app.get('/', (req, res) => {
     res.render('index');
   });
 
   app.get('/ux-ui', (req, res) => {
-    res.sendFile(path.join(__dirname, '../ux-ui.html'));
+    res.render('ux-ui');
   });
 
   app.get('/web-dev', (req, res) => {
-    res.sendFile(path.join(__dirname, '../web-dev.html'));
+    res.render('web-dev');
   });
 
   app.get('/android-app', (req, res) => {
