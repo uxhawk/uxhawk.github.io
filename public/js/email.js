550ec5195d4689b05b90ea963d3b7f13d9ace845
David Hawkins <uxhawk@gmail.com>
2020-05-26 09:47:09 -0400

    recaptcha works and displays confirmation message after successful response

--- public/js/email.js
+++ public/js/email.js
@@ -1,18 +1,48 @@
+/* eslint-disable no-unused-vars */
+let captchaRes;
+// eslint-disable-next-line no-unused-vars
+/**
+ * recaptcha function to check the user is not a robot
+ */
+function verifyRobot() {
+  captchaRes = grecaptcha.getResponse();
+}
+
 $(document).ready(function() {
   $('#email-btn').click(function(event) {
     event.preventDefault();
     const email = {
       from: $('#emailInput').val(),
       message: $('#emailBody').val(),
+      captcha: captchaRes,
     };
-    console.log(email.from);
+
+    const alert = $(`<div id="alert" class="alert alert-warning" role="alert">
+    Beep Boop! Please respond to the captcha.
+  </div>`);
+
+    const success = $(`<div id="success" 
+    class="alert alert-success mt-4" role="alert">
+    Message Sent. Thanks for the email!
+  </div>`);
+
     $.ajax({
       type: 'POST',
       data: email,
       url: '/api/send',
     }).then((data) => {
-      console.log(data);
-      location.reload();
+      if (data.success === false) {
+        $('#alert, #success').hide();
+        $('#recaptcha').prepend(alert);
+      } else if (data.success === true) {
+        $('#success').hide();
+        $('#emailMessage').append(success);
+        $('#alert').hide();
+        $('#emailInput').val('');
+        $('#emailBody').val('');
+        grecaptcha.reset();
+      }
     });
+    // verifyRobot();
   });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,25 +1,48 @@
 const sgMail = require('@sendgrid/mail');
+// const bodyParser = require('body-parser');
+const request = require('request');
 
 module.exports = function(app) {
   app.post('/api/send', (req, res) => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: 'uxhawk@gmail.com',
-      from: 'uxhawk@gmail.com',
-      subject: `You have a message from ${req.body.from}`,
-      text: req.body.message,
-    };
-    sgMail
-        .send(msg)
-        .then((data) => {
-          res.json(data);
-        }, (error) => {
-          console.error(error);
+    if (
+      req.body.captcha === undefined ||
+      req.body.captcha === '' ||
+      req.body.captcha === null
+    ) {
+      return res.json({success: false, msg: 'Please complete the captcha'});
+    }
 
-          if (error.response) {
-            console.error(error.response.body);
-          }
-        });
+    const verifyURL = `https://google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET}&response=${req.body.captcha}`;
+
+    // make request to Verify URL
+    request(verifyURL, (err, response, body) => {
+      body = JSON.parse(body);
+
+      // if not successful
+      if (body.success !== undefined && !body.success) {
+        return res.json({success: false, msg: 'Failed captcha verification'});
+      } else {
+        // if successfull
+        res.json({success: true, msg: 'Captcha passed'});
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+          to: 'uxhawk@gmail.com',
+          from: 'uxhawk@gmail.com',
+          subject: `You have a message from ${req.body.from}`,
+          text: req.body.message,
+        };
+        sgMail
+            .send(msg)
+            .then((data) => {
+              res.json(data);
+            }, (error) => {
+              console.error(error);
+
+              if (error.response) {
+                console.error(error.response.body);
+              }
+            });
+      }
+    });
   });
 };
-
